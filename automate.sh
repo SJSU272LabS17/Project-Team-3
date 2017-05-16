85bade0831599ed936e816e22aa1ce626f40ca8c
Ambika Bohra <bohraambika@yahoo.com>
2017-05-15 21:17:15 -0700

    oragnized project files

--- automate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Hi"
-eval "rm -f /Users/mulumoodi/Downloads/dl*.png"
-kill -9 $(ps | grep camserver2|grep node|head -n1 | awk '{print $1;}')
-eval "node camserver2.js" &
-eval "open sample.html"
