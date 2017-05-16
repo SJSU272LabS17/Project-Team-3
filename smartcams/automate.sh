bba6a6d31f959607e5a949adcf0a464d400b24de
Ambika Bohra <bohraambika@yahoo.com>
2017-05-15 21:08:27 -0700

    Changed automate script camserver.js

--- smartcams/automate.sh
+++ smartcams/automate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hi"
 eval "rm -f /Users/mulumoodi/Downloads/dl*.png"
-kill -9 $(ps | grep camserver2|grep node|head -n1 | awk '{print $1;}')
-eval "node camserver2.js" &
+kill -9 $(ps | grep camserver|grep node|head -n1 | awk '{print $1;}')
+eval "node camserver.js" &
 eval "open sample.html"
